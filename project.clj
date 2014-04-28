fe1a71c276da4bffcb359ae0a7cad2059041d3d4
Joel Kuiper <me@joelkuiper.eu>
2014-04-28 22:28:27 +0200

    Add project dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,15 @@
 (defproject spa "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :description "Mozilla PDF.js based viewer for machine learning and information retrieval on text documents"
+  :url "https://github.com/joelkuiper/spa"
+  :license {:name "GNU General Public License (GPL) v3"
+            :url "https://www.gnu.org/copyleft/gpl.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.cli "0.3.1"]
+                 [ch.qos.logback/logback-classic "1.1.2"]
+                 [org.clojure/tools.logging "0.2.6"]
+
+                 [prismatic/plumbing "0.2.2"]
+
+                 [thrift-clj "0.2.1"]
+                 [org.clojure/core.async "0.1.298.0-2a82a1-alpha"]
+                 [http-kit "2.1.18"]])
