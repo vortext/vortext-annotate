7122814bde38a9749feea3e517d1f0abaaec8043
Joel Kuiper <me@joelkuiper.eu>
2014-04-29 11:03:30 +0200

    Document (SpaDoc) definition and services

--- services/services.thrift
+++ services/services.thrift
@@ -2,3 +2,21 @@ namespace java spa.services.services
 
 include "structs.thrift"
 include "exceptions.thrift"
+
+service PDFParser {
+        void ping(),
+        // Calls the pdf parser to return, given a binary pdf, the RawPDF
+        Document parse(1:string binary)
+
+        oneway void shutdown()
+}
+
+service Filter {
+        void ping(),
+
+        // Filters are elements of a pipeline.
+        // They receieve a document and yield a different (modified) document
+        Document filter(1:Document document) throws (1:ProcessException error),
+
+        oneway void shutdown()
+}
--- services/structs.thrift
+++ services/structs.thrift
@@ -1,1 +1,41 @@
 namespace java spa.services.structs
+
+struct Interval {
+       1: required i32 upper;
+       2: required i32 lower;
+}
+
+struct TextNode {
+       1: i32 pageIndex;
+       2: Interval interval;
+}
+
+struct MetaReference {
+       1: string field;
+       2: i32 idx;
+}
+
+struct Annotation {
+       1: i64 uuid;
+       2: i32 label;
+       3: MetaReference __meta;
+}
+
+struct Marginalis {
+       1: string id;
+       2: string title;
+       3: string description;
+       4: list<Annotation> annotations;
+}
+
+struct Mapping {
+       1: list<i32> textNodes;
+       2: list<Interval> ranges;
+}
+
+struct Document {
+       1: required string text;
+       2: optional list<Marginalis> marginalia;
+       3: optional map<string, Mapping> __meta;
+       4: optional list<TextNode> __textNodes;
+}
