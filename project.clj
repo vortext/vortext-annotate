95b4cb7cf8bfb77b4b2f0c7f6a350986bbe5ff49
Joel Kuiper <me@joelkuiper.eu>
2014-05-06 12:54:11 +0200

    Begin handling of HTTP requests

--- project.clj
+++ project.clj
@@ -1,16 +1,26 @@
 (defproject spa "0.1.0-SNAPSHOT"
   :description "Mozilla PDF.js based viewer for machine learning and information retrieval on text documents"
   :url "https://github.com/joelkuiper/spa"
+  :main spa.core
   :license {:name "GNU General Public License (GPL) v3"
             :url "https://www.gnu.org/copyleft/gpl.html"}
+  :plugins [[lein-environ "0.5.0"]]
+  :env {:debug true
+        :port 8080}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.cli "0.3.1"]
-                 [ch.qos.logback/logback-classic "1.1.2"]
                  [org.clojure/tools.logging "0.2.6"]
+                 [org.clojure/core.async "0.1.298.0-2a82a1-alpha"]
+                 [org.clojure/tools.reader "0.8.4"]
+                 [ch.qos.logback/logback-classic "1.1.2"]
+
+                 [environ "0.5.0"]
+
+                 [http-kit "2.1.18"]
+                 [compojure "1.1.5"]
+                 [ring/ring-devel "1.2.1"]
 
                  [org.zeromq/jeromq "0.3.3"]
                  [org.zeromq/cljzmq "0.1.4" :exclusions [org.zeromq/jzmq]]
-                 [org.clojure/tools.reader "0.8.4"]
-                 [prismatic/plumbing "0.2.2"]
-                 [org.clojure/core.async "0.1.298.0-2a82a1-alpha"]
-                 [http-kit "2.1.18"]])
+
+                 [prismatic/plumbing "0.2.2"]])
--- src/spa/core.clj
+++ src/spa/core.clj
@@ -1,6 +1,44 @@
-(ns spa.core)
+(ns spa.core
+  (:gen-class)
+  (:use environ.core)
+  (:require [clojure.tools.logging :as log]
+            [ring.middleware.reload :as reload]
+            [ring.util.response :as response]
+            [org.httpkit.server :refer :all]
+            [compojure.core :refer :all]
+            [compojure.handler :as handler]
+            [compojure.route :as route]
+            [org.httpkit.server :refer :all]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defonce server (atom nil))
+
+(defn assemble-routes []
+  (->
+   (routes
+    (GET "/" [] (response/resource-response "index.html" {:root "public"}))
+    (route/resources "/static")
+    (route/not-found "Page not found"))))
+
+(def app
+  (->
+   (assemble-routes)
+   (handler/api)))
+
+(defn stop-server []
+  (log/info "stopping server on" (env :port) "by user request")
+  (when-not (nil? @server)
+    ;; graceful shutdown: wait for existing requests to be finished
+    (@server :timeout 100)
+    (reset! server nil))
+  (log/info "… bye bye"))
+
+(defn -main [& args]
+  ;; The #' is useful, when you want to hot-reload code
+  ;; You may want to take a look: https://github.com/clojure/tools.namespace
+  ;; and http://http-kit.org/migration.html#reload
+  (log/info "starting server, listening on" (env :port) (when (env :debug) "[DEBUG]"))
+  (.addShutdownHook (Runtime/getRuntime) (Thread. (fn [] (stop-server))))
+  (let [handler (if (env :debug)
+                  (reload/wrap-reload app) ;; only reload when dev
+                  app)]
+    (reset! server (run-server handler {:port (env :port)}))))
