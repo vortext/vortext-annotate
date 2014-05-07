9da8e176f918891678c148412cb5f3f3e996792a
Joel Kuiper <me@joelkuiper.eu>
2014-05-07 15:28:42 +0200

    Update README

--- resources/topologies/ebm/topology.clj
+++ resources/topologies/ebm/topology.clj
@@ -5,6 +5,7 @@
 ;; The topology MUST be defined and MUST be compilable by prismatic graph
 ;; The input to the topology is the raw HTTP POST payload, by convention called source
 ;; We only return the sink to the client. Make sure that all relevant results are present and it's in a format understood
+;; You MAY define custom serialization / deserialization
 (def topology
   {:source (fnk [body] (slurp body))
    :sink   (fnk [source] (call "python" "ebm.word_tokenizer" source))
