0656fbad9d6d9831d631d4ce27c6f377f4df2186
Joel Kuiper <me@joelkuiper.eu>
2014-05-16 12:27:17 +0200

    Add Example button

--- static/scripts/components/fileLoader.jsx
+++ static/scripts/components/fileLoader.jsx
@@ -1,5 +1,5 @@
 /* -*- mode: js2; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2; js2-basic-offset: 2 -*- */
-define(['react'], function(React) {
+define(['react', 'jQuery'], function(React, $) {
   'use strict';
 
   // from http://stackoverflow.com/questions/12092633/pdf-js-rendering-a-pdf-file-using-a-base64-file-source-instead-of-url
@@ -18,6 +18,24 @@ define(['react'], function(React) {
   }
 
   return React.createClass({
+    loadExample: function() {
+      var self = this;
+      // From http://stackoverflow.com/questions/18447468/jquery-ajax-downloading-incomplete-binary-file
+      var request = new XMLHttpRequest();
+      request.open("GET", "/static/examples/TestDocument3.pdf", true);
+      request.responseType = "arraybuffer";
+
+      request.onload = function (e) {
+        var arrayBuffer = request.response; // Note: not request.responseText
+        if (arrayBuffer) {
+          var byteArray = new Uint8Array(arrayBuffer);
+          for (var i = 0; i < byteArray.byteLength; i++) {
+            self.props.model.set({data:byteArray});
+          }
+        }
+      };
+      request.send(null);
+    },
     loadFile: function() {
       var self = this;
       var file = this.refs.file.getDOMNode().files[0];
@@ -36,8 +54,9 @@ define(['react'], function(React) {
     render: function() {
       return(
          <form enctype="multipart/form-data" onSubmit={this.loadFile}>
-           <input name="file" type="file" ref="file" />
-           <input type="submit" className="button secondary small" value="Upload" />
+          <input name="file" type="file" ref="file" />
+           <input onClick={this.loadExample} type="button" className="button secondary tiny" value="Example" />
+           <input type="submit" className="button tiny" value="Upload" />
          </form>);
     }
   });
