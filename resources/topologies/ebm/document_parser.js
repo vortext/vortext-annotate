09f3d89eecc131aa5d5e9771066c829a58b91a92
Joel Kuiper <me@joelkuiper.eu>
2014-05-28 14:15:50 +0200

    Remove base64 support in PDF.js on NodeJS
    Don’t really need it after all

--- resources/topologies/ebm/document_parser.js
+++ resources/topologies/ebm/document_parser.js
@@ -77,27 +77,15 @@ if(require.main === module) {
   program
     .option('-i, --input [input]', 'path to PDF to parse')
     .option('-o, --output [output]', 'path to output file, flushes to STDOUT if none given')
-    .option('-n, --noparse', 'Do no parse, return the base64 encoded pdf')
-    .option('-b, --base64', 'input pdf as base64')
     .parse(process.argv);
 
-  var pdf;
-  if(program.base64) {
-    var pdfData = fs.readFileSync(program.input, "binary");
-    pdf = new Uint8Array(Buffer(pdfData, "base64"));
-  } else {
-    pdf = new Uint8Array(fs.readFileSync(program.input));
-  }
+  var pdf = new Uint8Array(fs.readFileSync(program.input));
   var out = program.output;
-  if(program.noparse) {
-    flush(new Buffer(pdf, "binary").toString("base64"), out);
-  } else {
-    var resultPromise = converToDocument(pdf);
-    resultPromise.then(function(result) {
-      result = JSON.stringify(result);
-      flush(result, out);
-    });
-  }
+  var resultPromise = converToDocument(pdf);
+  resultPromise.then(function(result) {
+    result = JSON.stringify(result);
+    flush(result, out);
+  });
 }
 
 module.exports = handler;
