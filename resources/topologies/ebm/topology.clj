9a55af319d4c787e09370dbbd9e8ab54da5e5a21
Joel Kuiper <me@joelkuiper.eu>
2014-05-28 22:22:14 +0200

    Dreadful code to rejig the marginalia & nodes

--- resources/topologies/ebm/topology.clj
+++ resources/topologies/ebm/topology.clj
@@ -1,24 +1,33 @@
 (ns topologies.ebm ;; This MUST be in the form topologies.<name> and cannot contain special characters.
   (:require [spa.services :refer [call]]
             [cheshire.core :as json]
+            [clojure.tools.logging :as log]
             [clojure.java.io :as io])
   (:use plumbing.core))
 
 ;; The topology MUST be defined and MUST be compilable by prismatic graph.
 ;; The input to the topology is the Ring request, by convention called source.
 ;; We only return the sink to the client.
-;; Make sure sing returns a valid Ring response
+;; Make sure sink returns a valid Ring response
 ;; You MAY define custom serialization / deserialization, as none is done by default.
 ;; See the [Ring Spec](https://github.com/ring-clojure/ring/blob/master/SPEC)
 
 (def py (partial call :python))
 (def js (partial call :node))
 
-(defn merge-marginalia [doc]
+(defn collapse-annotations [doc]
   "merges marginalia with the node mappings and nodes"
-  doc)
+  (let [{:strs [marginalia nodes mapping]} doc]
+    (map (fn [{:strs [annotations] :as m}]
+           (let [new-annotations
+                 (map (fn [{{:strs [field index]} "mapping" :as a}]
+                        (let [mapped (get-in mapping [field index])
+                              nodes (map #(nth nodes (get % "node")) mapped)]
+                          (map merge mapped nodes))) annotations)]
+             (assoc-in m ["annotations"] new-annotations))
+           ) marginalia)))
 
-(def to-response (comp json/decode merge-marginalia json/encode))
+(def to-response (comp json/encode collapse-annotations json/decode)) ; right-to-left
 
 (def topology
   {:source        (fnk [body] (.bytes body))
