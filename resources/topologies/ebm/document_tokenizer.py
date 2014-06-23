6ea5616e260d8b99cf10280e349ecfe94e697633
Joel Kuiper <me@joelkuiper.eu>
2014-06-23 12:47:51 +0200

    Use scikit’s regular expression for word tokens

--- resources/topologies/ebm/document_tokenizer.py
+++ resources/topologies/ebm/document_tokenizer.py
@@ -1,4 +1,4 @@
-import logging, copy, sys
+import logging, copy, sys, re
 log = logging.getLogger(__name__)
 
 from document_handler import DocumentHandler
@@ -6,45 +6,6 @@ from document_handler import DocumentHandler
 from nltk.tokenize.punkt import *
 from functools import wraps
 from itertools import izip
-
-# custom adaptations of the nltk tokenizers to suit scientific article parsing
-class CustomLanguageVars(PunktLanguageVars):
-    _re_non_word_chars = r"(?:[?!)\";}\]\*:@\'\({\[=\.])" # added =
-    """Characters that cannot appear within words"""
-
-    _re_word_start = r"[^\(\"\`{\[:;&\#\*@\)}\]\-,=]" # added =
-    """Excludes some characters from starting word tokens"""
-
-class newPunktWordTokenizer(TokenizerI):
-    """
-    taken from new version of NLTK 3.0 alpha
-    to allow for span tokenization of words
-    (current full version does not allow this)
-    """
-    def __init__(self, lang_vars=CustomLanguageVars()):
-        self._lang_vars = lang_vars
-
-
-    def tokenize(self, text):
-        return self._lang_vars.word_tokenize(text)
-
-    def span_tokenize(self, text):
-        """
-        Given a text, returns a list of the (start, end) spans of words
-        in the text.
-        """
-        return [(sl.start, sl.stop) for sl in self._slices_from_text(text)]
-
-    def _slices_from_text(self, text):
-        last_break = 0
-        contains_no_words = True
-        for match in self._lang_vars._word_tokenizer_re().finditer(text):
-            contains_no_words = False
-            context = match.group()
-            yield slice(match.start(), match.end())
-            if contains_no_words:
-                yield slice(0, 0) # matches PunktSentenceTokenizer's functionality
-
 
 class OverlappingIntervals():
     """
@@ -80,7 +41,8 @@ class Handler(DocumentHandler):
 
     def __init__(self):
         log.info("constructing %s" % (self.title))
-        self.word_tokenizer = newPunktWordTokenizer()
+
+        self.word_token_pattern = re.compile(r"(?u)\b\w\w+\b")
         self.sentence_tokenizer = PunktSentenceTokenizer()
 
     def handle_document(self, document):
@@ -99,11 +61,12 @@ class Handler(DocumentHandler):
             sentence_nodes[-1]["range"][1] = sentence_span[1]
 
             sentence = text[sentence_span[0]:sentence_span[1]]
-            word_spans = self.word_tokenizer.span_tokenize(sentence)
             sentence_overlap = OverlappingIntervals([node["range"] for node in sentence_nodes])
 
             # Add word mappings
-            for word_span in word_spans:
+            for m in self.word_token_pattern.finditer(sentence):
+                word_span = m.span()
+                # Add sentence offset
                 word_nodes = sentence_overlap.overlap_indices([x+sentence_span[0] for x in word_span])
 
                 word_nodes[0]["range"][0] = word_span[0]
@@ -111,7 +74,6 @@ class Handler(DocumentHandler):
 
                 word_mappings += [word_nodes]
             sentence_mappings += [sentence_nodes]
-
 
         document.update({
             "mapping": {
