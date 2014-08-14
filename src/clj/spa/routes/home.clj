0fb822d7d963015f225d379672729d3b592d9314
Joel Kuiper <me@joelkuiper.eu>
2014-08-14 14:59:36 +0200

    Do not cache the home page

--- src/clj/spa/routes/home.clj
+++ src/clj/spa/routes/home.clj
@@ -4,15 +4,16 @@
             [noir.response :as response]
             [spa.layout :as layout]))
 
+(def no-cache {"Cache-Control" "no-cache, no-store, must-revalidate"
+               "Pragma" "no-cache"
+               "Expires" "0"})
+
 (defn home-page []
   (if (session/get :user-id)
     (response/redirect "/projects")
-    (layout/render "home.html" {:page-type "home"
-                                :logged-out? (session/flash-get :logged-out)})))
-
-(defn about-page []
-  (layout/render "about.html"))
+    (->> (layout/render-to-response "home.html" {:page-type "home"
+                                 :logged-out? (session/flash-get :logged-out)})
+       (response/set-headers no-cache))))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+  (GET "/" [] (home-page)))
