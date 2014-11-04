031995bf2d63b311f8f4914fbbc1e6149c5dbdc2
Joel Kuiper <me@joelkuiper.eu>
2014-11-04 13:49:15 +0100

    Add insert-fragments
    Also see this discussion http://lists.w3.org/Archives/Public/public-openannotation/2014Nov/0001.html

--- src/clj/spa/semantic/annotations.clj
+++ src/clj/spa/semantic/annotations.clj
@@ -5,26 +5,36 @@
    [spa.semantic.sparql :as sparql])
   (:import [java.net URI URL]))
 
-(def select-query
-  (slurp (io/file (io/resource "sparql/select-document.sparql"))))
-
 (defn uuid [] (str (java.util.UUID/randomUUID)))
 
-(defn document-iri
-  [document-id]
-  (URI. (str (:base-uri env) "documents/" document-id)))
+(defn get-query
+  [name]
+  (slurp (io/file (io/resource (str "sparql/" name ".sparql")))))
 
-(defn project-iri
-  [project-id]
-  (URI. (str (:base-uri env) "projects/" project-id)))
+(def select-query (get-query "select-document"))
+(def insert-annotation-fragment (get-query "insert-annotation-fragment"))
+(def insert-description-fragment (get-query "insert-description-fragment"))
+
+(defn uuid-iri []
+  (URI. (str "urn:uuid:" (uuid))))
+
+(defn iri-with-base
+  [type]
+  (URI. (str (:base-uri env) type "/" document-id)))
+
+(defn document-iri [document-id] (iri-with-base "documents"))
+(defn project-iri [document-id] (iri-with-base "projects"))
 
 (defn select-document
   [project-id document-id]
   (let [document-iri (document-iri document-id)
         project-iri (project-iri project-id)
-        bindings {"projectGraphIri" project-iri
-                  "documentIri" document-iri}]
+        bindings {"project" project-iri
+                  "_document" document-iri}]
     (sparql/result->clj (sparql/select (:sparql-query env) select-query bindings))))
 
+(defn insert-document
+  [project-id document-id marginalia])
+
 (defn update-document
-  [project-id document-id annotations])
+  [project-id document-id marginalia])
