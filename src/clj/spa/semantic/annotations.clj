3721c63c85ef9fd09589120498cbbe0919b674e2
Joel Kuiper <me@joelkuiper.eu>
2014-11-05 15:11:21 +0100

    Fixes to the semantic representation of the OA

--- src/clj/spa/semantic/annotations.clj
+++ src/clj/spa/semantic/annotations.clj
@@ -12,26 +12,69 @@
   (slurp (io/file (io/resource (str "sparql/" name ".sparql")))))
 
 (def select-query (get-query "select-document"))
-(def insert-annotation-fragment (get-query "insert-annotation-fragment"))
+(def insert-highlight-fragment (get-query "insert-highlight-fragment"))
 (def insert-description-fragment (get-query "insert-description-fragment"))
+(def insert-concept-fragment (get-query "insert-concept-fragment"))
 
 (defn uuid-iri [] (URI. (str "urn:uuid:" (uuid))))
-(defn iri-with-base [type] (URI. (str (:base-uri env) type "/" document-id)))
+(defn iri-with-base [type] (URI. (str (:base-uri env) type "/")))
 
-(defn document-iri [document-id] (iri-with-base "documents"))
-(defn project-iri [document-id] (iri-with-base "projects"))
+(defn document-iri [project-id document-id]
+  (URI. (str (iri-with-base "documents") "projects/" project-id "/documents/" document-id)))
+(defn project-iri [project-id]
+  (URI. (str (iri-with-base "projects") project-id)))
+(defn concept-iri [concept-name]
+  (URI. (str (iri-with-base "concepts") concept-name)))
 
 (defn select-document
   [project-id document-id]
-  (let [document-iri (document-iri document-id)
-        project-iri (project-iri project-id)
-        bindings {"project" project-iri
-                  "_document" document-iri}]
-    (sparql/result->clj
-     (sparql/select (:sparql-query env) select-query bindings))))
+  (sparql/result->clj
+   (sparql/select
+    (:sparql-query env)
+    select-query
+    {"project"(project-iri project-id)
+     "documentRegex" (str "^" (document-iri project-id document-id))})))
+
+(defn- insert-concept-query
+  [project-id concept-name label]
+  (sparql/query-with-bindings
+   insert-concept-fragment
+   {"project" (project-iri project-id)
+    "concept" (concept-iri concept-name)
+    "label" label}))
+
+(defn- insert-highlight-query
+  ([project-id document-id concept quote suffix prefix]
+     (let [document (document-iri project-id document-id)]
+       (sparql/query-with-bindings
+        insert-highlight-fragment
+        {"project" (project-iri project-id)
+         "document" document
+         "annotation" (uuid-iri)
+         "concept" concept
+         "target" (URI. (str document "#" (uuid)))
+         "selector" (uuid-iri)
+         "prefix" prefix
+         "suffix" suffix
+         "quote" quote})))
+  ([project-id document-id concept quote]
+     (insert-highlight-query project-id document-id concept quote "" "")))
+
+(defn- insert-description-query
+  [project-id document-id concept description]
+  (sparql/query-with-bindings
+   insert-description-fragment
+   {"project" (project-iri project-id)
+    "document" (document-iri document-id)
+    "annotation" (uuid-iri)
+    "concept" concept
+    "descriptionBody" (uuid-iri)
+    "description" description}))
 
 (defn insert-document
   [project-id document-id marginalia])
 
+
+;; This is extremely stupid, but it will simply throw out everything and insert new stuff
 (defn update-document
   [project-id document-id marginalia])
